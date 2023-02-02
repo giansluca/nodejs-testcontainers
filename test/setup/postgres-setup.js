fff28271c7f3c26748d59f01be6842e509d9410e
gians <giansluca@gmail.com>
2023-02-02 11:30:13 +0100

    update pg image

--- test/setup/postgres-setup.js
+++ test/setup/postgres-setup.js
@@ -1,14 +1,14 @@
 const { PostgreSqlContainer } = require("testcontainers");
-const { types, Client } = require('pg')
+const { types, Client } = require("pg");
 
 const postgresStart = async () => {
-    const postgresDb = await new PostgreSqlContainer("postgres:14.2").start();
+    const postgresDb = await new PostgreSqlContainer("postgres:15.1").start();
     return postgresDb;
 };
 
 const getPostgresClient = () => {
-    types.setTypeParser(1700, x => parseFloat(x));
+    types.setTypeParser(1700, (x) => parseFloat(x));
     return new Client(process.env._POSTGRES_URL);
-}
+};
 
 module.exports = { postgresStart, getPostgresClient };
