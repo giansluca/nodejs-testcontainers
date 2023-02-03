bac4b75228fb90126444f2de631c942016e585f7
gians <giansluca@gmail.com>
2023-02-03 14:06:25 +0100

    update

--- test/setup/elasticsearch-setup.js
+++ test/setup/elasticsearch-setup.js
@@ -2,9 +2,11 @@ const { ElasticsearchContainer } = require("testcontainers");
 const { Client } = require("@elastic/elasticsearch");
 
 const elasticStart = async () => {
-    const elasticsearchDb = await new ElasticsearchContainer("elasticsearch:8.2.0")
-        .withEnv("discovery.type", "single-node")
-        .withEnv("xpack.security.enabled", "false")
+    const elasticsearchDb = await new ElasticsearchContainer("elasticsearch:8.6.1")
+        .withEnvironment({
+            "discovery.type": "single-node",
+            "xpack.security.enabled": "false",
+        })
         .start();
 
     return elasticsearchDb;
@@ -14,10 +16,10 @@ const getElasticClient = () => {
     const clientConfig = {
         node: process.env._ELASTICSEARCH_URL,
         auth: null,
-        ssl: false
+        ssl: false,
     };
 
     return new Client(clientConfig);
-}
+};
 
 module.exports = { elasticStart, getElasticClient };
