bc058aa04967ba139cab981694c2ddf74a7a5415
gians <giansluca@gmail.com>
2023-02-28 23:52:24 +0100

    update

--- src/start.js
+++ src/start.js
@@ -1,8 +1,8 @@
-const config = require("./config")
+const config = require("./config");
 
 const startApp = () => {
     console.log("Hello! this is a start :D");
-    console.log(config.utils.testUrl)
+    console.log(config.utils.testUrl);
 };
 
 module.exports = { startApp };
